78f6ee5c012461095d7faaaf0edcb2421ec1f701
Tom Donaldson <tdonaldson@stsci.edu>
2014-06-26 16:55:35 -0400

    initial plots

--- javascripts/scatter_plot.js
+++ javascripts/scatter_plot.js
@@ -31,6 +31,10 @@ function initcb(tableModel, tabletop) {
     sheets = tableModel;
     sites = tableModel.Sites;
     
+    
+    xDropDown = d3.select("#Xselector");
+    yDropDown = d3.select("#Yselector");
+    
     finishInit();
 }
 
@@ -39,8 +43,6 @@ function finishInit () {
     // Set selectors with column names.
     var keys = sites.column_names;
 
-    var xDropDown = d3.select("#Xselector");
-    var yDropDown = d3.select("#Yselector");
     
     xDropDown.selectAll('option').remove();
     yDropDown.selectAll('option').remove();
@@ -52,16 +54,18 @@ function finishInit () {
     } 
     
     // Set selector initial values.
-    var xAxisCol = sites.column_names[5];
-    var yAxisCol = sites.column_names[8];
-    
+    var xAxisCol = sites.column_names[4];
+    var yAxisCol = sites.column_names[5];
+    xDropDown.node().value = xAxisCol;    
+    yDropDown.node().value = yAxisCol;
     
     
     var svg = d3.select("#fig1").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr('id', 'svg');
     
     svg.append("g")
         .attr("class", "x axis")
@@ -78,28 +82,59 @@ function finishInit () {
         .style("text-anchor", "end")
         .text("Price ($)");
 
-
+    replot_data();
 }
 function onload() {
     initdata();
 }
 
 function change_x_axis(value) {
-    console.log("New X axis is: " + value);
+    replot_data();
 
 }
 
 function change_y_axis(value) {
-    console.log("New Y axis is: " + value);
+    replot_data();
 }
 
 
 function replot_data() {
+    var xCol = xDropDown.node().value;
+    var yCol = yDropDown.node().value;
+    
+    var xData = function(d) {
+            return d[xCol]
+            };
+    var yData = function(d) {
+            return d[yCol]
+            };
+            
+    
+    var a = sites.elements;
+    
+    
+    // Set the domains for these columns
+    var xmin = d3.min(a, xData);
+    var xmax = d3.max(a, xData);
+    x.domain([xmin, xmax]);
+    
+    var ymin = d3.min(a, yData);
+    var ymax = d3.max(a, yData);
+    y.domain([ymin, ymax]);
+
+        
+    var svg = d3.select("#svg");
+    
+    svg.select('.xaxis').call(xAxis);
+    svg.select('.yaxis').call(yAxis);
+
+
     svg.selectAll("circle")
-        .data(data).enter()
+        .data(a).enter()
         .append("svg:circle")
+        .attr('fill', 'white')
         .attr("r", 4)
-        .attr("cx", function(d) { return data(d) })
-        .attr("cy", function(d) { return data(d) });
+        .attr("cx", xData)
+        .attr("cy", yData);
 }
 
