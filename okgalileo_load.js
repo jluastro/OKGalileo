58e53db55c97fd67e7229d41ad1745f0d31046d6
Sarah Kendrew <sarahaskendrew@gmail.com>
2014-06-26 20:22:35 +0100

    global variable in table init file

--- okgalileo_load.js
+++ okgalileo_load.js
@@ -3,6 +3,8 @@ window.onload = function() { initdata() };
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1c2ZuB_FYI0uj-f2a_OSG3uK_OsyCpdgeVSXAUrVqkR8/pubhtml';
 
 function initdata() {
-    Tabletop.init( { key: public_spreadsheet_url,
-                simpleSheet: true } );
+    sites=Tabletop.init( { key: public_spreadsheet_url,
+                simpleSheet: false },
+                wanted=["Sites"] 
+                parseNumbers=true);
     }
